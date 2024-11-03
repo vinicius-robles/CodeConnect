0dbb8bd35a9dd16522dfb9dbe2f566d43282b300
vinicius-robles <vinicius.robles@gmail.com>
2024-11-03 16:16:21 -0300

    Foi construida toda a estrutura HTML e estilização CSS da página e iniciada a lógica JS de carregamento de imagem.

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const uploadBtn = document.getElementById('upload-btn');
+const inputUpload = document.getElementById('imagem-upload');
+
+uploadBtn.addEventListener('click', () => {
+    inputUpload.click();
+})
