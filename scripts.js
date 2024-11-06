a90cb65fd01cd9f39f1335cfaa7e3f43d911f0d3
vinicius-robles <vinicius.robles@gmail.com>
2024-11-06 19:38:01 -0300

    Adiciona a lógica de carregamento de arquivo, atualizando a imagem e o nome do que foi carregado.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,36 @@
-const uploadBtn = document.getElementById('upload-btn');
-const inputUpload = document.getElementById('imagem-upload');
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("imagem-upload");
 
-uploadBtn.addEventListener('click', () => {
+uploadBtn.addEventListener("click", () =>{
     inputUpload.click();
+})
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name});
+        }
+        leitor.onerror = () => {
+            reject(`Erro de leitura do arquivo ${arquivo.name}`);
+        }
+        leitor.readAsDataURL(arquivo);
+    })
+}
+
+const imagemPrincipal = document.querySelector('.main-imagem');
+const nomeDaImagem = document.querySelector('.container-imagem-nome p');
+
+inputUpload.addEventListener('change', async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch(erro) {
+            console.error('Erro na leitura do arquivo');
+        }
+    }
 })
